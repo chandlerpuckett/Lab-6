839f8f80380da1eb335cdf7e50302e33559a1034
Chandler Puckett <chandler.puckett1@gmail.com>
2020-08-17 15:31:42 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// ===== packages ===== //
+
+const express = require('express');
+require ('dontenv').config();
+const cors = require('cors');
+
+// ===== global variables ===== //
+
+const PORT = process.env.PORT || 3003;
+const app = express();
+app.use(cors());
+
+// ===== routes ===== //
+
+
+
+
+
+
+// ===== start the server ===== //
+
+app.listen(PORT, () => console.log(`we are running on PORT : ${PORT}`));
