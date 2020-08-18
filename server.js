f61f67d4af9c1757d0f62d082120d061ebdb68a8
Chandler Puckett <chandler.puckett1@gmail.com>
2020-08-18 14:10:45 -0700

    PR

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function sendLocation (request,response){
   const jsonLocationObject = require('./data/location.json');
   const city = request.query.city;
 
-  // take out ffasdkfmasodk lab 7
+  // take out ffasdkaksdgkands ksjksdkdskd lab7
   if (city !== 'lynnwood'){
     return response.status(500).send('we only have `Lynnwood`');
   }
